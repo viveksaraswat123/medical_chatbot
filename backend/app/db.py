40d4f1d08292e4a47015184278f80c23b63ce143
viveksaraswat49 <viveksaraswat49@gmail.com>
2026-03-09 00:20:18 +0530

    modified db file for production

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,29 +1,14 @@
 import os
 import certifi
 from pymongo import MongoClient
-from dotenv import load_dotenv
-
-load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 
-if not MONGO_URI:
-    raise ValueError("MONGO_URI not found in environment variables!")
-
-try:
-    client = MongoClient(
-        MONGO_URI,
-        tlsCAFile=certifi.where(),
-        serverSelectionTimeoutMS=5000
-    )
-
-    # Test connection
-    client.admin.command("ping")
-    print("MongoDB Atlas connection successful!")
-
-except Exception as e:
-    print("MongoDB connection failed:", e)
-    raise
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
 
 db = client["medibot_db"]
 
